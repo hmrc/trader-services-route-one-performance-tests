95584be85c9f073dbea662f23a81890c762eba44
Tasmine10ten <55190161+Tasmine10ten@users.noreply.github.com>
2021-03-01 10:31:08 +0000

    update amend change

--- src/test/scala/uk/gov/hmrc/perftests/traderServices/TraderServicesSimulation.scala
+++ src/test/scala/uk/gov/hmrc/perftests/traderServices/TraderServicesSimulation.scala
@@ -165,7 +165,7 @@ class TraderServicesSimulation extends PerformanceTestRunner {
 
     getWhichAmend,
     pause,
-    postWriteOnly,
+    postWriteAndUpload,
     getWriteResponsePage,
     pause,
     postFreeTextResponse,
