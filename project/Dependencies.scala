b76b8dc80922d637b0fccdefb3f3fe01a3f2e822
baha.aboukoura <baha.aboukoura@digital.hmrc.gov.uk>
2024-01-22 16:47:13 +0000

    upgrade dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,11 +2,11 @@ import sbt._
 
 object Dependencies {
 
-  private val gatlingVersion = "3.4.2"
+  private val gatlingVersion = "3.5.1"
 
   val test = Seq(
     "com.typesafe"          % "config"                    % "1.4.2"        % Test,
-    "uk.gov.hmrc"          %% "performance-test-runner"   % "5.3.0"        % Test,
+    "uk.gov.hmrc"          %% "performance-test-runner"   % "5.5.0"        % Test,
     "io.gatling"            % "gatling-test-framework"    % gatlingVersion % Test,
     "io.gatling.highcharts" % "gatling-charts-highcharts" % gatlingVersion % Test
   )
